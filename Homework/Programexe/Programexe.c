5f93eca5ae1b2a9fc0274be5ba5798627331b83e
VladimirSdobnov <114135450+VladimirSdobnov@users.noreply.github.com>
2022-10-22 13:18:41 +0300

    Третья дз вариант 1 доделанный

--- Homework/Programexe/Programexe.c
+++ Homework/Programexe/Programexe.c
@@ -42,23 +42,20 @@ int main() {
 
 	printf("Добро пожаловать!");
 	Sleep(500);
-	system("cls");
-	printf("Загрузка . ");
-	Sleep(500);
-	printf(". ");
-	Sleep(500);
-	printf(".");
-	Sleep(500);
-	system("cls");
-	printf("Загрузка . ");
-	Sleep(500);
-	printf(". ");
-	Sleep(500);
-	printf(".");
-	Sleep(500);
-	system("cls");
-
-	int choose = 1000, a, b;
+	for (int i = 0; i < 2; i++) {
+		system("cls");
+		printf("Загрузка ");
+		Sleep(500);
+		printf(". ");
+		Sleep(500);
+		printf(". ");
+		Sleep(500);
+		printf(".");
+		Sleep(500);
+		system("cls");
+	}
+	
+	int choose = 1000, a, b, r;
 	while (choose != 0) {
 		printf("Выбор фигуры: \n");
 		printf("1. Прямоугольник.\n");
@@ -137,5 +134,42 @@ int main() {
 				}
 			}
 		}
+		else if (choose == 3) {
+			system("cls");
+			printf("Введите радиус окружности: ");
+			scanf_s("%d", &r);
+			system("cls");
+			while (choose < 4) {
+				printf("Меню:\n");
+				printf("1. Найти площадь круга.\n");
+				printf("2. Найти длину окружности круга.\n");
+				printf("3. Найти диаметр круга.\n");
+				printf("4. Вернуться к выбору фигуры.\n");
+				printf("0. Выйти.\n");
+				printf("Ввод: ");
+				scanf_s("%d", &choose);
+				if (choose == 1) {
+					printf("Площадь круга: %.2f\n", sqr_ball(r));
+					system("pause");
+					system("cls");
+				}
+				else if (choose == 2) {
+					printf("Длина окружности круга: %.2f\n", perimetr_ball(r));
+					system("pause");
+					system("cls");
+				}
+				else if (choose == 3) {
+					printf("Диаметр круга: %d\n", diametr(r));
+					system("pause");
+					system("cls");
+				}
+				else {
+					system("cls");
+					break;
+				}
+			}
+		}
+		system("cls");
 	}
+	system("pause");
 }
